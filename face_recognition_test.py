fc5eab08595037d33695cdefdcee9bc2cf5bcbac
Carl <carlsebpogi123@gmail.com>
2024-10-01 15:33:15 +0800

    Added attendance_logging.py and attendance_images; no variable changes implemented yet

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -6,7 +6,7 @@ import face_recognition
 imgElon = face_recognition.load_image_file('test_images/Elon Musk.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('test_images/Elon Test.jpg')
-imgTest = c.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
 
 faceLoc = face_recognition.face_locations(imgElon)[0]
